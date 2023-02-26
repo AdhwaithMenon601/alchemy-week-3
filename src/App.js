d08e5ea21305542d49ed79b3e331eb5b6d3e1884
Adhwaith Menon <adhu@Adhwaiths-MacBook-Pro.local>
2023-02-26 16:29:09 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -21,16 +21,51 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [finalBlocksState, setFinalBlockState] = useState({
+    finalBlocks: [],
+    selected: null,
+  });
+  const [moreInfoSelectedBlock, setMoreInfoSelectedBlock] = useState();
+  const [showTransaction, setShowTransaction] = useState();
+  const [totalBalance, setTotalBalance] = useState(0);
+
+  const handleSelectBlock = async (block) => {
+    let moreInfoAboutBlock = await alchemy.core.getBlockWithTransactions(block);
+    setMoreInfoSelectedBlock(moreInfoAboutBlock);
+    setFinalBlockState({ ...finalBlocksState, selected: block });
+  };
+
+  const handleGetBalance = async (e) => {
+    e.preventDefault();
+    try {
+      let getBalance = await alchemy.core.getBalance(
+        e.target[0].value,
+        "latest"
+      );
+      setTotalBalance(Utils.formatUnits(getBalance));
+    } catch (error) {
+      console.log(error);
+      setTotalBalance(0);
+      alert("Wrong address!!");
+    }
+    console.log(e.target[0].value);
+  };
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      let lastBlockNumber = await alchemy.core.getBlockNumber();
+      setBlockNumber(lastBlockNumber);
+      let arr = [];
+      for (let i = lastBlockNumber; i >= lastBlockNumber - 6; i--) {
+        arr.push(i);
+      }
+      setFinalBlockState({ ...finalBlocksState, finalBlocks: arr });
     }
 
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (<div className="App">Block Number: {blockNumber}</div>);
 }
 
 export default App;
